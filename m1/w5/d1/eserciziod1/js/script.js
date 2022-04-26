9ed8347bd0f55d5371b4ae21de18594d5fe182b5
GDrinkwater9 <giorgio.bevilacqua00@gmail.com>
2022-04-26 17:31:19 +0200

    esercizio-w5-d1

--- m1/w5/d1/eserciziod1/js/script.js
+++ m1/w5/d1/eserciziod1/js/script.js
@@ -1,10 +1,13 @@
 let data = new Date();
 
 let giorno = data.getDate();
+console.log(giorno)
 
-let mese = data.getMonth()
+let mese = data.getMonth()+1;
+console.log(mese)
 
 let anno = data.getFullYear();
+console.log(anno)
 
 document.write(giorno + '/' + mese + '/' + anno)
 
@@ -18,14 +21,14 @@ let orologio = new Date();
 
 
 let futureMinutes = orologio.getHours ;orologio.getMinutes();
-orologio.setMinutes( orologio);
+orologio.setMinutes(orologio);
+
+setInterval(function(){
+    let n = new Date();
+    document.querySelector('#orologio').innerHTML = n.getHours() + ' : '+ n.getMinutes() + ' : '
+    + n.getSeconds()
+})
 
 
-
-    setInterval(function(){
-        let n = new Date();
-        document.querySelector('#orologio').innerHTML = n.getHours() + ' : '+ n.getMinutes() + ' : '
-        + n.getSeconds()
-    })
 
  
