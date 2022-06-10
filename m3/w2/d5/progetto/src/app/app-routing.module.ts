63236d55ff62bf34c74636ac2e86699598c90442
GDrinkwater9 <giorgio.bevilacqua00@gmail.com>
2022-06-10 17:24:11 +0200

    progetto

---
+++ m3/w2/d5/progetto/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+{
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+ 
+  { path: '',
+    pathMatch: 'full',
+
+  loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, 
+ 
+  { path: 'register',
+
+ loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
